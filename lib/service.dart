25ff506909c611221be14c536f1ca4c87629aac0
panshen <415391360@qq.com>
2020-04-15 18:55:43 +0800

    first commit

---
+++ lib/service.dart
@@ -0,0 +1,22 @@
+/// base
+const njqbaseUrl = "https://appbg.lcfarm.com";
+
+const homeNoviceListUrl = "/api/product/list/noviceProductList.htm";
+
+/// regularList ()
+/// 参数: currentPage=1
+const homeRegularListUrl = "/api/product/list/regularProductList.htm";
+
+/// AD
+/// params: adType:homeAd (homeBanner,homeAd,homeBottom)
+const adUrl = "/api/mediaAnnouncement/getAdInfo.htm";
+
+
+//BB
+const bbBaseUrl = "http://wuhan.yxybb.com";
+
+const bbContext = "/BXEXAMOL/restservices/api";
+
+const getExamSubject = "/examol_web_getcheckExamListByCardno";
+
+const suffix = '/query';
